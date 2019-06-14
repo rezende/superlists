a0e2c543431ee434ef117a38f172e582492eb8a6
Vinicius Rezende <vgrezende@gmail.com>
2019-06-14 12:16:31 -0300

    More stuff

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,18 +1,17 @@
+import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
-import time
-import unittest
 
 MAX_WAIT = 10
 
-class NewVisitorTest(StaticLiveServerTestCase):  
+class NewVisitorTest(StaticLiveServerTestCase):
 
-    def setUp(self):  
+    def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def tearDown(self):  
+    def tearDown(self):
         self.browser.quit()
 
     def wait_for_row_in_list_table(self, row_text):
@@ -23,18 +22,18 @@ class NewVisitorTest(StaticLiveServerTestCase):
                 rows = table.find_elements_by_tag_name('tr')
                 self.assertIn(row_text, [row_text for row in rows])
                 return
-            except (AssertionError, WebDriverException) as e:
+            except (AssertionError, WebDriverException) as exc:
                 if time.time() - start_time > MAX_WAIT:
-                    raise e
+                    raise exc
                 time.sleep(0.5)
 
-    def test_can_start_a_list_for_one_user(self):  
+    def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)  
+        self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
@@ -82,7 +81,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # She notices that her list has a unique URL
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+') 
+        self.assertRegex(edith_list_url, '/lists/.+')
 
         # Now a new user, Francis, comes along to the site.
 
